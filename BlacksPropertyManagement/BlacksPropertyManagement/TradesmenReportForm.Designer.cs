e811c80353f5fd9bf68bbf4cfe8229aa54714166
guywhohasagun <52847538+guywhohasagun@users.noreply.github.com>
2024-08-26 23:19:31 +1200

    Finished report

--- BlacksPropertyManagement/BlacksPropertyManagement/TradesmenReportForm.Designer.cs
+++ BlacksPropertyManagement/BlacksPropertyManagement/TradesmenReportForm.Designer.cs
@@ -29,13 +29,56 @@ namespace BlacksPropertyManagement
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TradesmenReportForm));
+            this.btnPrint = new System.Windows.Forms.Button();
+            this.btnReturn = new System.Windows.Forms.Button();
+            this.printJobs = new System.Drawing.Printing.PrintDocument();
+            this.prvJobs = new System.Windows.Forms.PrintPreviewDialog();
             this.SuspendLayout();
+            // 
+            // btnPrint
+            // 
+            this.btnPrint.Location = new System.Drawing.Point(54, 138);
+            this.btnPrint.Name = "btnPrint";
+            this.btnPrint.Size = new System.Drawing.Size(216, 70);
+            this.btnPrint.TabIndex = 0;
+            this.btnPrint.Text = "Print Report";
+            this.btnPrint.UseVisualStyleBackColor = true;
+            this.btnPrint.Click += new System.EventHandler(this.btnPrint_Click);
+            // 
+            // btnReturn
+            // 
+            this.btnReturn.Location = new System.Drawing.Point(329, 138);
+            this.btnReturn.Name = "btnReturn";
+            this.btnReturn.Size = new System.Drawing.Size(216, 70);
+            this.btnReturn.TabIndex = 1;
+            this.btnReturn.Text = "Return";
+            this.btnReturn.UseVisualStyleBackColor = true;
+            this.btnReturn.Click += new System.EventHandler(this.btnReturn_Click);
+            // 
+            // printJobs
+            // 
+            this.printJobs.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.printJobs_PrintPage);
+            // 
+            // prvJobs
+            // 
+            this.prvJobs.AutoScrollMargin = new System.Drawing.Size(0, 0);
+            this.prvJobs.AutoScrollMinSize = new System.Drawing.Size(0, 0);
+            this.prvJobs.ClientSize = new System.Drawing.Size(400, 300);
+            this.prvJobs.Document = this.printJobs;
+            this.prvJobs.Enabled = true;
+            this.prvJobs.Icon = ((System.Drawing.Icon)(resources.GetObject("prvJobs.Icon")));
+            this.prvJobs.Name = "prvJobs";
+            this.prvJobs.Visible = false;
             // 
             // TradesmenReportForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.btnReturn);
+            this.Controls.Add(this.btnPrint);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "TradesmenReportForm";
             this.Text = "Tradesmen Report";
             this.ResumeLayout(false);
@@ -43,5 +86,10 @@ namespace BlacksPropertyManagement
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnPrint;
+        private System.Windows.Forms.Button btnReturn;
+        private System.Drawing.Printing.PrintDocument printJobs;
+        private System.Windows.Forms.PrintPreviewDialog prvJobs;
     }
 }
--- BlacksPropertyManagement/BlacksPropertyManagement/TradesmenReportForm.cs
+++ BlacksPropertyManagement/BlacksPropertyManagement/TradesmenReportForm.cs
@@ -14,6 +14,9 @@ namespace BlacksPropertyManagement
     {
         private DataController DC;
         private MainForm frmMenu;
+        private int amountOfTradesmanPrinted, pagesAmountExpected;
+        private DataRow[] tradesmanForPrint;
+
 
         public TradesmenReportForm(DataController dc, MainForm mnu)
         {
@@ -21,6 +24,146 @@ namespace BlacksPropertyManagement
             DC = dc;
             frmMenu = mnu;
             frmMenu.Hide();
+
+        }
+
+        private void printJobs_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            int linesSoFar = 0;
+
+            Font textFont = new Font("Arial", 10, FontStyle.Regular);
+            Font textFontCenter = new Font("Arial", 10, FontStyle.Regular);
+            Font headingFont = new Font("Arial", 16, FontStyle.Bold);
+
+            DataRow drTradesman = tradesmanForPrint[amountOfTradesmanPrinted];
+
+            CurrencyManager cmProperty, cmLandlord, cmTradesman, cmMaterial;
+
+            cmProperty = (CurrencyManager)this.BindingContext[DC.dsBlacksProperty, "PROPERTY"];
+            cmTradesman = (CurrencyManager)this.BindingContext[DC.dsBlacksProperty, "TRADESMAN"];
+            cmLandlord = (CurrencyManager)this.BindingContext[DC.dsBlacksProperty, "LANDLORD"];
+            cmMaterial = (CurrencyManager)this.BindingContext[DC.dsBlacksProperty, "MATERIAL"];
+
+            Brush brush = new SolidBrush(Color.Black);
+
+            int leftMargin = e.MarginBounds.Left;
+            int topMargin = e.MarginBounds.Top;
+            int headingLeftMargin = 50;
+            int topMarginDetails = topMargin + 70;
+            int rightMargin = e.MarginBounds.Right;
+
+            string headingText = "Black's Property Management";
+
+            int centre = (e.PageBounds.Width - headingText.Length) / 2;
+
+            g.DrawString(headingText, headingFont, brush, centre - leftMargin, topMargin + headingFont.Height * linesSoFar);
+
+            linesSoFar++;
+
+            headingText = "     Tradesmen Report      ";
+
+            g.DrawString(headingText, headingFont, brush, centre - leftMargin, topMargin + headingFont.Height * linesSoFar);
+            linesSoFar = linesSoFar + 2;
+
+            linesSoFar = linesSoFar + 3;
+
+            g.DrawString("Tradesman ID: " + drTradesman["TradesmanID"], textFont, brush, leftMargin + headingLeftMargin, topMargin + (linesSoFar * textFont.Height));
+            linesSoFar++;
+            g.DrawString("Name: " + drTradesman["TradesmanLastName"] + ", " + drTradesman["TradesmanFirstName"] + "             Fee:  $" + drTradesman["Fee"], textFont, brush, leftMargin + headingLeftMargin, topMargin + (linesSoFar * textFont.Height));
+            linesSoFar++;
+            g.DrawString("Phone number " + drTradesman["PhoneNumber"], textFont, brush, leftMargin + headingLeftMargin, topMargin + (linesSoFar * textFont.Height));
+
+            DataRow[] drTradesmans = drTradesman.GetChildRows(DC.dtTradesman.ChildRelations["TRADESMANJOB"]);
+
+            linesSoFar = linesSoFar + 2;
+
+            if (drTradesmans.Length == 0)
+            {
+                g.DrawString("This Trademan has no jobs assigned currently", textFont, brush,
+                               leftMargin + headingLeftMargin, topMargin + (linesSoFar * textFont.Height));
+            }
+            else
+            {
+                foreach (DataRow drJobs in drTradesmans)
+                {
+                    int aPropertyID = Convert.ToInt32(drJobs["PropertyID"]);
+                    cmProperty.Position = DC.propertyView.Find(aPropertyID);
+                    DataRow drProperty = DC.dtProperty.Rows[cmProperty.Position];
+
+                    g.DrawString("Job ID: " + drJobs["JobID"] + "        Date: " + drJobs["JobDate"] + "        Job Status: " + drJobs["JobStatus"], textFont, brush, leftMargin + headingLeftMargin, topMargin + (linesSoFar *
+                                             textFont.Height));
+                    linesSoFar++;
+
+                    g.DrawString("Description: " + drJobs["JobDescription"] + "        Fee: $" + drJobs["JobFee"], textFont, brush, leftMargin + headingLeftMargin, topMargin + (linesSoFar * textFont.Height));
+
+                    linesSoFar = linesSoFar + 2;
+
+                    g.DrawString("Property ID:" + drProperty["PropertyID"] + "   Address: " + drProperty["StreetAddress"] + ", " + drProperty["Suburb"], textFont, brush, leftMargin + headingLeftMargin, topMargin + (linesSoFar * textFont.Height));
+
+                    linesSoFar++;
+
+                    g.DrawString("Year Built: " + drProperty["YearBuilt"], textFont, brush, leftMargin + headingLeftMargin, topMargin + (linesSoFar * textFont.Height));
+
+                    linesSoFar = linesSoFar + 2;
+
+                    double materialsTotal = 0;
+
+                    g.DrawString("ID  Description    Cost     Quantity        Price", textFont, brush,
+                                               leftMargin + headingLeftMargin, topMargin + (linesSoFar * textFont.Height));
+                    linesSoFar++;
+
+                    DataRow[] drJobMaterials = drJobs.GetChildRows(DC.dtJob.ChildRelations["JOB_JOBMATERIAL"]);
+                                
+                                
+                    if (drJobMaterials.Length == 0)
+                    {
+                        g.DrawString("This Job has no Materials assigned currently", textFont, brush,
+                               leftMargin + headingLeftMargin, topMargin + (linesSoFar * textFont.Height));
+                    }
+                    else
+                    {
+                        foreach (DataRow drJobMaterial in drJobMaterials)
+                        {
+                            int aMaterialID = Convert.ToInt32(drJobMaterial["MaterialID"]);
+                            cmMaterial.Position = DC.materialView.Find(aMaterialID);
+                            DataRow drMaterial = DC.dtMaterial.Rows[cmMaterial.Position];
+                            string materialText = "";
+                            double materialPrice = Convert.ToDouble(drMaterial["Cost"]) * Convert.ToInt32(drJobMaterial["Quantity"]);
+                            materialsTotal += materialPrice;
+                            materialText = drJobMaterial["MaterialID"].ToString() + "  " + drMaterial["MaterialDescription"] + "    $" + drMaterial["Cost"] + "     " + drJobMaterial["Quantity"] + "       $" + string.Format("{0:#,##0.00}", materialPrice);
+                            g.DrawString(materialText, textFont, brush, leftMargin + headingLeftMargin, topMargin + (linesSoFar * textFont.Height));
+                            linesSoFar++;
+                        }
+                    }
+                    linesSoFar = linesSoFar + 3;
+                }
+            }
+
+            amountOfTradesmanPrinted++;
+            if (!(amountOfTradesmanPrinted == pagesAmountExpected))
+            {
+                e.HasMorePages = true;
+            }
+
+        }
+
+        private void btnReturn_Click(object sender, EventArgs e)
+        {
+            Close();
+            frmMenu.Show();
+
+        }
+
+        private void btnPrint_Click(object sender, EventArgs e)
+        {
+            amountOfTradesmanPrinted = 0;
+            string strFilter = "";
+            string strSort = "TradesmanID";
+            tradesmanForPrint = DC.dsBlacksProperty.Tables["TRADESMAN"].Select(strFilter, strSort, DataViewRowState.CurrentRows);
+            pagesAmountExpected = tradesmanForPrint.Length;
+            prvJobs.Show();
 
         }
     }
